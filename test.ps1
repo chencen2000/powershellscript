d0a79fdb051ba046c9eb8d6ea5883ebbee1d3a94
chencen2000 <34563412+chencen2000@users.noreply.github.com>
2018-07-21 14:58:46 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,5 @@
+<#
+.\BrowsingHistoryView.exe /HistorySource 1 /sxml history.xml
+Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/chencen2000/powershellscript/master/test.ps1 -OutFile test.ps1
+#>
 Write-Output "Hello,world."
